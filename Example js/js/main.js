813ed62e7d9eee9c636ecdfde28e45fe09d3b7e3
ziyoxoja <89178858+ziyoxoja-06@users.noreply.github.com>
2021-11-13 11:41:25 +0500

    JavaScript

---
+++ Example js/js/main.js
@@ -0,0 +1,11 @@
+let main = 4
+let con = document.getElementById("con")
+switch (main) {
+    case 1:
+        case 2:case 3:case 4:case 5:case 6:case 7:
+        console.log(con.innerHTML+=`${main} - masala`);
+        break;
+
+    default:
+        break;
+}
